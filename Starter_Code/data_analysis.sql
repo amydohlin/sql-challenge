5f23f26e3a993dc167f54322eda36a12aa858e7b
Amy Dohlin <153555607+amydohlin@users.noreply.github.com>
2024-05-24 21:12:45 -0500

    erd and schemas
    erd, importing into pgadmin, begin data analysis

---
+++ Starter_Code/data_analysis.sql
@@ -0,0 +1,22 @@
+-- Check that tables/data were imported correctly.
+select *
+from departments;
+
+select *
+from dept_emp;
+
+select *
+from dept_manager;
+
+select *
+from employees;
+
+select *
+from salaries;
+
+select *
+from titles;
+
+
+
+
